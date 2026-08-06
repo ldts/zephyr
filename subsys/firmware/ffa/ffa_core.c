dd4c8f02787fef3ae6f7d4ca6a5b2542dddf8602
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2026-08-06 16:19:52 +0200

    subsys: firmware: add FF-A core Kconfig and build scaffolding
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

---
+++ subsys/firmware/ffa/ffa_core.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2026 Qualcomm Innovation Center, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+/* Real implementation is added in subsequent tasks. */
